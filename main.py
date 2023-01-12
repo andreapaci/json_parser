493b252a6f4790e78cb077599d23e1bbf923a1f3
andrea <andrea.paci1998@gmail.com>
2023-01-12 18:19:04 +0100

    Added two utility functions and tests

--- main.py
+++ main.py
@@ -6,18 +6,17 @@ indent_char = '  '
 container_char_dict = '$'
 container_char_list = '#'
 
-#TODO: Testare bene la funzione modify by path e find by pattern
 
-# Recursive function
+# Recursive function: This function modify a value of a json field accessing with an index
 # Returns True are set in order to prematurely exit the function once it has modified what it has to.
 # If the function returns True, it means it has found what he had to, otherwise it hasn't found it.
-def json_modify_by_index(data, index, new_val=None, lst=None):
+def json_modify_by_index(data, index, new_val, lst=None):
     if lst is None:
         lst = [0]
     if isinstance(data, dict):
         for (k, v) in data.items():
             if isinstance(v, dict) or isinstance(v, list):
-                if json_modify_by_index(v, index, new_val=new_val , lst=lst):
+                if json_modify_by_index(v, index, new_val, lst=lst):
                     return True
             else:
                 if index == lst[0]:
@@ -27,7 +26,7 @@ def json_modify_by_index(data, index, new_val=None, lst=None):
     elif isinstance(data, list):
         for i in range(0, len(data)):
             if isinstance(data[i], dict) or isinstance(data[i], list):
-                if json_modify_by_index(data[i], index, new_val=new_val , lst=lst):
+                if json_modify_by_index(data[i], index, new_val, lst=lst):
                     return True
             else:
                 if index == lst[0]:
@@ -38,25 +37,70 @@ def json_modify_by_index(data, index, new_val=None, lst=None):
         raise Exception("Json decoding went wrong!")
     return False
 
+
+# Recursive function. Return the value of a json field accessed by index
+def json_access_by_index(data, index, lst=None):
+    if lst is None:
+        lst = [0]
+    if isinstance(data, dict):
+        for (k, v) in data.items():
+            if isinstance(v, dict) or isinstance(v, list):
+                ret = json_access_by_index(v, index, lst=lst)
+                if ret is not None:
+                    return ret
+            else:
+                if index == lst[0]:
+                    return data[k]
+                lst[0] = lst[0] + 1
+    elif isinstance(data, list):
+        for i in range(0, len(data)):
+            if isinstance(data[i], dict) or isinstance(data[i], list):
+                ret = json_access_by_index(data[i], index, lst=lst)
+                if ret is not None:
+                    return ret
+            else:
+                if index == lst[0]:
+                    return data[i]
+                lst[0] = lst[0] + 1
+    else:
+        raise Exception("Json decoding went wrong!")
+    return None
+
+
 def json_modify_by_path(data, path, new_val, index=None):
     if path == "":
         data[index] = new_val
         return
     dict_pos = path.find(container_char_dict)
     list_pos = path.find(container_char_list)
-    if (dict_pos > 0 and list_pos > 0 and dict_pos < list_pos) or (list_pos < 0 and dict_pos > 0):
+    if (0 < dict_pos < list_pos and list_pos > 0) or (list_pos < 0 and dict_pos > 0):
         tokens = path.split(container_char_dict, 1)
         if index is None:
             return json_modify_by_path(data, tokens[1], new_val, index=tokens[0])
         else:
             return json_modify_by_path(data[index], tokens[1], new_val, index=tokens[0])
-    if (list_pos > 0 and dict_pos > 0 and list_pos < dict_pos) or (dict_pos < 0 and list_pos > 0):
+    if (0 < list_pos < dict_pos and dict_pos > 0) or (dict_pos < 0 and list_pos > 0):
         tokens = path.split(container_char_list, 1)
         if index is None:
             return json_modify_by_path(data, tokens[1], new_val, index=int(tokens[0]))
         else:
             return json_modify_by_path(data[index], tokens[1], new_val, index=int(tokens[0]))
     raise Exception("Path accessing went wrong")
+
+
+def json_access_by_path(data, path):
+    if path == "":
+        return data
+    dict_pos = path.find(container_char_dict)
+    list_pos = path.find(container_char_list)
+    if (0 < dict_pos < list_pos and list_pos > 0) or (list_pos < 0 and dict_pos > 0):
+        tokens = path.split(container_char_dict, 1)
+        return json_access_by_path(data[tokens[0]], tokens[1])
+    if (0 < list_pos < dict_pos and dict_pos > 0) or (dict_pos < 0 and list_pos > 0):
+        tokens = path.split(container_char_list, 1)
+        return json_access_by_path(data[int(tokens[0])], tokens[1])
+    raise Exception("Path accessing went wrong")
+
 
 def json_find_by_pattern(data, pattern, path=""):
     if isinstance(data, dict):
@@ -81,6 +125,7 @@ def json_find_by_pattern(data, pattern, path=""):
                     return new_path
     else:
         raise Exception("Json decoding went wrong!")
+    return None
 
 
 def json_parse(data, indent=0, lst=None, path=""):
@@ -101,10 +146,10 @@ def json_parse(data, indent=0, lst=None, path=""):
             else:
                 if isinstance(v, str):
                     print("\"" + str(v) + "\",", "(" + str(lst[0]) + ")", new_path)
-                    #print("\"" + str(v) + "\",")
+                    # print("\"" + str(v) + "\",")
                 else:
                     print(str(v) + ",", "(" + str(lst[0]) + ")", new_path)
-                    #print(str(v) + ",")
+                    # print(str(v) + ",")
                 lst[0] = lst[0] + 1
     elif isinstance(data, list):
         for i in range(0, len(data)):
@@ -120,28 +165,39 @@ def json_parse(data, indent=0, lst=None, path=""):
             else:
                 if isinstance(data[i], str):
                     print(indent_char * indent + "\"" + str(data[i]) + "\",", "(" + str(lst[0]) + ")", new_path)
-                    #print(indent_char * indent + "\"" + str(data[i]) + "\",")
+                    # print(indent_char * indent + "\"" + str(data[i]) + "\",")
                 else:
                     print(indent_char * indent + str(data[i]) + ",", "(" + str(lst[0]) + ")", new_path)
-                    #print(indent_char * indent + str(data[i]) + ",")
+                    # print(indent_char * indent + str(data[i]) + ",")
                 lst[0] = lst[0] + 1
     else:
         raise Exception("Json decoding went wrong!")
 
 
-
 if __name__ == '__main__':
     data = json.loads(jsonString)
-    json_parse(data)
 
-    #print(json_modify_by_path(data, "popup$menuitem$0#onclick$"))
-    #print(json_modify_by_path(data, "popup$menuitem$1#value$2#onclick$"))
-    #print(json_modify_by_path(data, "id$"))
-    #print(json_modify_by_path(data, "popup$menuitem$0#value$"))
-    json_modify_by_path(data, json_find_by_pattern(data, "addreSS$2#oncliCk$1"), "orcamado")
-
+    print("------------------------------------------------------------")
     json_parse(data)
-#    for i in range(0,26):
-#        print("----------------------")
-#        json_modify_by_index(data, i, new_val=1)
-#        json_parse(data)
+    print("------------------------------------------------------------")
+
+
+    for i in range(0, 18, 2):
+        assert json_modify_by_index(data, i, i)
+        assert i == json_access_by_index(data, i)
+        print(i, ":", json_access_by_index(data, i))
+
+    assert not json_modify_by_index(data, 120, 120)
+    assert json_access_by_index(data, 120) is None
+
+    pattern_val = [["addreSS$2#oncliCk$1", "A"], ["address$1#onclick$", "B"], ["id", 1], ["$menuitem$address$2#onclick$0#", 1337]]
+    for e in pattern_val:
+        json_modify_by_path(data, json_find_by_pattern(data, e[0]), e[1])
+        print(json_access_by_path(data, json_find_by_pattern(data, e[0])))
+        assert e[1] == json_access_by_path(data, json_find_by_pattern(data, e[0]))
+
+    assert json_find_by_pattern(data, "xxxxxxxxxxxxxxx") is None
+
+    print("------------------------------------------------------------")
+    json_parse(data)
+    print("------------------------------------------------------------")
