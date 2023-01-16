6131d3db3a7c3a6fbcb3f07b0c023eb679bb90b6
andrea <andrea.paci1998@gmail.com>
2023-01-16 20:15:19 +0100

    Fixed strict type for json_key_val.py and json_key.py.

--- json_key.py
+++ json_key.py
@@ -1,6 +1,6 @@
 # Class used to store the tuple (key, accepted_values). Used in json_parse library to check whether a json contains certain key, value pairs
 class Json_Key:
 
-    def __init__(self, _key, _is_pattern):
+    def __init__(self, _key: str, _is_pattern: bool):
         self.key = _key #Key can be an exact path (key1$key2$3#key4$) or just a pattern (3#key4)
         self.is_pattern = _is_pattern
--- json_key_val.py
+++ json_key_val.py
@@ -1,7 +1,7 @@
 # Class used to store the tuple (key, accepted_values). Used in json_parse library to check whether a json contains certain key, value pairs
 class Json_Key_Val:
 
-    def __init__(self, _key, _is_pattern, _val):
+    def __init__(self, _key: str, _is_pattern: bool, _val):
         self.key = _key #Key can be an exact path (key1$key2$3#key4$) or just a pattern (3#key4)
         self.is_pattern = _is_pattern
         self.val = _val
