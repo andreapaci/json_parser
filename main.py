fbb1f3f2d0a90318cbe24e883615878ffe18ede4
andrea <andrea.paci1998@gmail.com>
2023-01-16 17:33:51 +0100

    Added the possibility to check for values and some more tests/fixes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import json_key_val as jkv
 
 jsonString = '{"id": "file","value": "File","popup":{"menuitem": { "ADDRESS":[{"value": "New", "onclick": "CreateNewDoc()"}, {"value": [{"value": "New", "onclick": "CreateNewDoc()"}, {"value": "Open", "onclick": "OpenDoc()"},{"value": {"id": "file","value": "File","popup":{"menuitem": { "ADDRESS":[{"value": "New", "onclick": "CreateNewDoc()"}, {"value": [{"value": "New", "onclick": "CreateNewDoc()"}, {"value": "Open", "onclick": "OpenDoc()"},{"value": "Close", "onclick": "CloseDoc()"}], "onclick": "OpenDoc()"},{"value": "Close", "onclick": [1,"2",3,4,{"value": "New", "onclick": "CreateNewDoc()"}]}]}}}}]}]}}}'
 indent_char = '  '
@@ -69,22 +70,30 @@ def json_access_by_index(data, index, lst=None):
 def json_modify_by_path(data, path, new_val, index=None):
     if path == "":
         data[index] = new_val
-        return
+        return True
     dict_pos = path.find(container_char_dict)
     list_pos = path.find(container_char_list)
     if (0 < dict_pos < list_pos and list_pos > 0) or (list_pos < 0 and dict_pos > 0):
         tokens = path.split(container_char_dict, 1)
         if index is None:
+            if tokens[0] not in data:
+                return False
             return json_modify_by_path(data, tokens[1], new_val, index=tokens[0])
         else:
+            if tokens[0] not in data[index]:
+                return False
             return json_modify_by_path(data[index], tokens[1], new_val, index=tokens[0])
     if (0 < list_pos < dict_pos and dict_pos > 0) or (dict_pos < 0 and list_pos > 0):
         tokens = path.split(container_char_list, 1)
         if index is None:
+            if int(tokens[0]) >= len(data):
+                return False
             return json_modify_by_path(data, tokens[1], new_val, index=int(tokens[0]))
         else:
+            if int(tokens[0]) >= len(data[index]):
+                return False
             return json_modify_by_path(data[index], tokens[1], new_val, index=int(tokens[0]))
-    raise Exception("Path accessing went wrong")
+    return False
 
 
 def json_access_by_path(data, path):
@@ -94,11 +103,15 @@ def json_access_by_path(data, path):
     list_pos = path.find(container_char_list)
     if (0 < dict_pos < list_pos and list_pos > 0) or (list_pos < 0 and dict_pos > 0):
         tokens = path.split(container_char_dict, 1)
+        if tokens[0] not in data:
+            return None
         return json_access_by_path(data[tokens[0]], tokens[1])
     if (0 < list_pos < dict_pos and dict_pos > 0) or (dict_pos < 0 and list_pos > 0):
         tokens = path.split(container_char_list, 1)
+        if int(tokens[0]) >= len(data):
+            return None
         return json_access_by_path(data[int(tokens[0])], tokens[1])
-    raise Exception("Path accessing went wrong")
+    return None
 
 
 def json_find_by_pattern(data, pattern, path=""):
@@ -136,6 +149,7 @@ def search_elem_in_keys(e, keys, lst):
         res = res and v
     return res
 
+
 # Recursive function: This function returns True if all the keys specified in "keys" are found in data, False otherwise
 def json_key_find(data, keys, lst=None):
     if lst is None:
@@ -158,6 +172,24 @@ def json_key_find(data, keys, lst=None):
         raise Exception("Json decoding went wrong!")
     return False
 
+
+# This function checks if the specified keys of an input json have the same values passed in input
+def json_key_value_compare(data, key_value):
+    # For each element of the key values...
+    for kv in key_value:
+        # If the keys doesn't exist, return false
+        key = kv.key
+        if kv.is_pattern:
+            key = json_find_by_pattern(data, kv.key)
+            if key is None:
+                return False
+        val = json_access_by_path(data, key)
+        res = False
+        for v in kv.val:
+            res = res or val == v
+        if not res:
+            return False
+    return True
 
 
 def json_parse(data, indent=0, lst=None, path=""):
@@ -213,29 +245,56 @@ if __name__ == '__main__':
     json_parse(data)
     print("------------------------------------------------------------")
 
-
     for i in range(0, 18, 2):
         assert json_modify_by_index(data, i, i)
         assert i == json_access_by_index(data, i)
-        print(i, ":", json_access_by_index(data, i))
+        #print(i, ":", json_access_by_index(data, i))
 
     assert not json_modify_by_index(data, 120, 120)
     assert json_access_by_index(data, 120) is None
 
-    pattern_val = [["addreSS$2#oncliCk$1", "A"], ["address$1#onclick$", "B"], ["id", 1], ["$menuitem$address$2#onclick$0#", 1337]]
+    pattern_val = [["addreSS$2#oncliCk$1", "A"], ["address$1#onclick$", "B"], ["id", 1],
+                   ["$menuitem$address$2#onclick$0#", 1337]]
     for e in pattern_val:
         json_modify_by_path(data, json_find_by_pattern(data, e[0]), e[1])
-        print(json_access_by_path(data, json_find_by_pattern(data, e[0])))
         assert e[1] == json_access_by_path(data, json_find_by_pattern(data, e[0]))
+
+    assert json_access_by_path(data, "xxxxxxxxx") is None
+    assert not json_modify_by_path(data, "xxxxxxxxx", "1")
 
     assert json_find_by_pattern(data, "xxxxxxxxxxxxxxx") is None
 
     assert json_key_find(data, ["address", "id"])
     assert json_key_find(data, ["adDress", "MENUITEM"])
-    assert not json_key_find(data, ["address", "caccapupu"])
+    assert not json_key_find(data, ["address", "notexisting"])
+    assert not json_key_find(data, ["notexisting"])
     assert json_key_find(data, [])
 
     assert isinstance(json_access_by_path(data, "popup$menuitem$ADDRESS$1#value$2#value$popup$menuitem$ADDRESS$2#onclick$"), list)
+
+    key_val = [jkv.Json_Key_Val("popup$menuitem$ADDRESS$1#value$2#value$popup$menuitem$ADDRESS$2#onclick$0#", False, ["A", "12", 1337]),
+               jkv.Json_Key_Val("popup$menuitem$ADDRESS$1#value$2#value$popup$menuitem$ADDRESS$2#onclick$3#", True, ["A", 4, 1337]),
+               jkv.Json_Key_Val("id", True, ["cane", "example", "12", 1])]
+
+    assert json_key_value_compare(data, key_val)
+
+    key_val = [jkv.Json_Key_Val("popup$menuitem$ADDRESS$1#value$2#value$popup$menuitem$ADDRESS$2#onclick$0#", False, ["A", 1337, "12"]),
+               jkv.Json_Key_Val("popup$menuitem$ADDRESS$1#value$2#value$popup$menuitem$ADDRESS$2#onclick$2#", True, ["A", 87878, 3, 1337]),
+               jkv.Json_Key_Val("id", True, ["cane", "example", "12", "file"])]
+
+    assert not json_key_value_compare(data, key_val)
+
+    key_val = [jkv.Json_Key_Val("popup$menuitem$ADDRESS$1#value$2#value$popup$menuitem$ADDRESS$2#onclick$0#", False, [1, 1, "A", 1337]),
+               jkv.Json_Key_Val("menuitem$ADDRESS$2#onclick$0#", True, ["A", 87878, 1337]),
+               jkv.Json_Key_Val("id", True, ["cane", "example", 1, "file"])]
+
+    assert json_key_value_compare(data, key_val)
+
+    key_val = [jkv.Json_Key_Val("address$0#value$", True, [2, "12", "New"]),
+               jkv.Json_Key_Val("dsadasdsdsa#dsadsadsa#", True, ["A", 87878]),
+               jkv.Json_Key_Val("id", True, ["cane", "example", 1, "file"])]
+
+    assert not json_key_value_compare(data, key_val)
 
     print("------------------------------------------------------------")
     json_parse(data)
